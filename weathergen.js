b799a47f2a7157fcb569e641c580f6a4dc95e8e8
Sophie Helf <sophie.helf@gmail.com>
2016-07-05 13:07:43 -0700

    fixed box width for mobile

--- weathergen.js
+++ weathergen.js
@@ -15,14 +15,14 @@ $(document).ready(function() {
       var far = "It's currently " + Math.floor((data.main.temp * (9 / 5)) - 459.7) + "°F ";
       var cel = "It's currently " + Math.floor(data.main.temp - 273.15) + "°C ";
      var conditions = data.weather[0].main;
-     var id = data.weather.id;
+     var id = data.weather[0].id;
       
 
       // show location, temp, and weather conditions
 
       $("#location").html("You're in " + location);
       $("#temp").html(far);
-      $("#conditions").html("The weather is " + conditions.toLowerCase());
+      $("#conditions").html("Today's weather: " + conditions.toLowerCase());
 
       // click button to toggle between fahrenheit and celsius
 
@@ -34,18 +34,20 @@ $(document).ready(function() {
       }); 
       
       // change container details dependent on weather conditions lmao this didn't take 10000000 years or anything
-      if (id > 199 && id <= 232) {
-	$("#container").css("background-color", "#333333");
-        $("#container").css("border", "black solid 1px");
+
+      if (id >= 199 && id <= 232) {
+  $("#container").css("background-color", "#333333");
+        $("#container").css("border", "yellow solid 2px");
         $("#location").css("color", "yellow");
         $("#temp").css("color", "yellow");
         $("#conditions").css("color", "yellow");
         $("button").css("color", "yellow");
         $("button").css("background-color", "#333333");
         $("button").css("border", "yellow solid 1px");
+        console.log(id);
 }
 else if (id >= 300 && id <= 531) {
-	$('#container').css("background-color", "#cccccc");
+  $('#container').css("background-color", "#cccccc");
         $("#container").css("border", "black dotted 1px");
         $("#location").css("color", "#000066");
         $("#temp").css("color", "#000066");
@@ -53,19 +55,21 @@ else if (id >= 300 && id <= 531) {
         $("button").css("background-color", "#333333");
         $("button").css("color", "white");
         $("button").css("border", "black solid 1px");
+        console.log(id);
 }
 else if (id >= 600 && id <= 622) {
-	$("#container").css("background-color", "#ffffff");
+  $("#container").css("background-color", "#ffffff");
         $("#container").css("border", "blue dotted 1px");
         $("#location").css("color", "blue");
         $("#temp").css("color", "blue");
         $("#conditions").css("color", "blue");
         $("button").css("background-color", "#ffffff");
         $("button").css("color", "blue");
-        $("button").css("border", "blue dotted 1px");
+        $("button").css("border", "blue dotted 1px"); 
+        console.log(id);
 }
 else if (id >= 700 && id <= 781) {
-	$("#container").css("background-color", "#ffffff");
+  $("#container").css("background-color", "#ffffff");
         $("#container").css("border", "#333333 dotted 1px");
         $("#location").css("color", "#333333");
         $("#temp").css("color", "#333333");
@@ -73,19 +77,21 @@ else if (id >= 700 && id <= 781) {
         $("button").css("background-color", "#333333");
         $("button").css("color", "white");
         $("button").css("border", "#8c8c8c dotted 1px");
+        console.log(id);
 }
 else if (id = 800) {
-	$('#container').css("background-color", "#99ccff");
-        $("#container").css("border", "blue dotted 1px");
+  $('#container').css("background-color", "#99ccff");
+        $("#container").css("border", "blue dotted 2px");
         $('#location').css("color", "black");
         $('#temp').css("color", "black");
         $('#conditions').css("color", "black");
         $("button").css("background-color", "white");
         $('button').css("border", "blue solid 1px");
         $("button").css("color", "blue");
+        console.log(id);
 }
 else if (id >= 801 && id <= 804) {
-	$("#container").css("background-color", "#ffffff");
+  $("#container").css("background-color", "#ffffff");
         $("#container").css("border", "#333333 dotted 1px");
         $("#location").css("color", "#333333");
         $("#temp").css("color", "#333333");
@@ -93,9 +99,10 @@ else if (id >= 801 && id <= 804) {
         $("button").css("background-color", "#333333");
         $("button").css("color", "white");
         $("button").css("border", "#8c8c8c dotted 1px");
+        console.log(id);
 }
 else if (id >=900 && id <= 906) {
-	 $("#container").css("background-color", "#ff3300");
+   $("#container").css("background-color", "#ff3300");
         $("#container").css("border", "black solid 1px");
         $("#location").css("color", "black");
         $("#temp").css("color", "black");
@@ -103,6 +110,7 @@ else if (id >=900 && id <= 906) {
         $("button").css("color", "white");
         $("button").css("background-color", "black");
         $("button").css("border", "black solid 1px");
+        console.log(id);
 }
       
     })
